93425f925d9d7d34ab375298ea7a607c225f0756
Seungyeol <marudev4905@gmail.com>
2022-01-28 03:07:38 +0900

    '.'

--- js/btnClick.js
+++ js/btnClick.js
@@ -12,3 +12,14 @@ $(".project-tab-btn").click(function(){
         behavior: "smooth"
     });
 });
+
+
+$(".bookmark-btn").click(function(){
+    var th = $(this);
+
+    if(th.hasClass("active")){
+        th.removeClass("active");
+    }else{
+        th.addClass("active");
+    }
+});
